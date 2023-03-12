4071216793cf494362417349e67ffa6c4ce1ee48
fajar260305 <100895588+fajar260305@users.noreply.github.com>
2023-03-12 12:56:22 +0700

    Update sidebar.blade.php

--- resources/views/komponen/sidebar.blade.php
+++ resources/views/komponen/sidebar.blade.php
@@ -11,14 +11,16 @@
         <li class="dropdown active">
           <a href="#" class="nav-link has-dropdown"><i class="fas fa-fire"></i><span>Dashboard</span></a>
         </li>
+        @if (Auth::user()->role == 'admin')
         <li class="dropdown ">
           <a href="/paket" class="nav-link "><i class="fas fa-fire"></i><span>Paket</span></a>
         </li>
         <li class="dropdown ">
-          <a href="/pesanan" class="nav-link "><i class="fas fa-fire"></i><span>Pesanan</span></a>
-        </li>
-        <li class="dropdown ">
           <a href="/pengguna" class="nav-link "><i class="fas fa-fire"></i><span>Pengguna</span></a>
+        </li>
+        @endif
+        <li class="dropdown ">
+          <a href="/pesanan" class="nav-link "><i class="fas fa-fire"></i><span>Pesanan</span></a>
         </li>
         <li class="dropdown ">
             <form action="/logout" method="POST">
